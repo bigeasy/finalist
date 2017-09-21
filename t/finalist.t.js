98c182377a03d8120ad7bdb2f411f12fc911e228
Alan Gutierrez <alan@prettyrobots.com>
2017-09-21 04:51:03 -0500

    Implementation.

--- t/finalist.t.js
+++ t/finalist.t.js
@@ -2,5 +2,11 @@ require('proof')(1, prove)
 
 function prove (okay) {
     var finalist = require('..')
-    okay(finalist, 'require')
+
+    finalist(function (callback) {
+        callback(null, 1)
+        callback(null, 3)
+    }, function (error, value) {
+        okay(value, 1, 'finalist')
+    })
 }
